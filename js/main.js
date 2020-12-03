2b8d52387be28b527539db3accc214ee7695251b
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-12-03 13:12:18 +0100

    Milestones - 02, 03

--- js/main.js
+++ js/main.js
@@ -5,26 +5,68 @@ const myApp = new Vue({
   data: {
     searchInput: '',
     language: 'it-IT',
-    searchMovies: [],
-    movieImage: 'https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png',
+    movies: [],
+    searchPage: 1,
+    moviePosterImage: 'https://image.tmdb.org/t/p/w185',
   },
+
+  mounted(){
+    axios.get('https://api.themoviedb.org/3/movie/popular', {
+      params: {
+        'api_key': API_KEY,
+        language: this.language,
+      }
+    }).then(popular => this.movies = popular.data.results);
+  },
+
   methods:{
     searchMovie(){
       console.log(this.searchInput);
       if (this.searchInput === '') {
 
       } else {
-        axios.get('https://api.themoviedb.org/3/search/multi', {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             'api_key': API_KEY,
             language: this.language,
             query: this.searchInput,
+            page: this.searchPage,
           }
-        }).then(r => {
-          console.log(r.data.results)
-          this.searchMovies = r.data.results
+        }).then(mov => {
+          this.movies = mov.data.results
+          console.log(mov.data.results);
+        });
+
+        axios.get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            'api_key': API_KEY,
+            language: this.language,
+            query: this.searchInput,
+            page: this.searchPage,
+          }
+        }).then(tv => {
+          tv.data.results.forEach(movie => this.movies.push(movie));
+          console.log(tv.data.results);
+          console.log(this.movies)
         });
       };
     },
+
+    // Tranforms vote from 1 to 10 into 1 to 5 stars
+    ratingStars(vote){
+      return Math.ceil(vote/2);
+    },
+
+    //image to show when flag is not found
+    errorFlag(e){
+        e.target.src = 'https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'
+    },
+
   },
+
+  computed: {
+  },
+
+
+
 });

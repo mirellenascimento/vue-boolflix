438aeb3cab3d38444938ed8b8193fe6b156eb64c
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-12-04 15:12:49 +0100

    Miestone - 04

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 const API_KEY = 'bbba21a7230b4c2b7a98694c2cb6c828';
+const homeMovies= [
+  {
+    name: 'Now Playing',
+    url: 'now_playing'
+  },
+  {
+    name: 'Popular',
+    url: 'popular'
+  },
+  {
+    name: 'Top rated',
+    url: 'top_rated'
+  },
+  {
+    name: 'Upcoming',
+    url: 'upcoming'
+  }
+];
+
+const languagesList = [
+  {
+    value: 'en-US',
+    image: 'en.svg',
+    name: 'English(USA)'
+  },
+  {
+    value: 'it-IT',
+    image: 'it.svg',
+    name: 'Italian'
+  },
+  {
+    value: 'fr-FR',
+    image: 'fr.svg',
+    name: 'French'
+  },
+]
 
 const myApp = new Vue({
   el: '#root',
   data: {
     searchInput: '',
     language: 'it-IT',
+    languagesList: [...languagesList],
     movies: [],
+    homeMovies: [...homeMovies],
+    genreMovies: [],
+    showingMovies: '',
     searchPage: 1,
-    moviePosterImage: 'https://image.tmdb.org/t/p/w185',
+    moviePosterImage: 'https://image.tmdb.org/t/p/w342',
+
+
   },
 
   mounted(){
@@ -17,6 +59,16 @@ const myApp = new Vue({
         language: this.language,
       }
     }).then(popular => this.movies = popular.data.results);
+
+
+
+    axios.get('https://api.themoviedb.org/3/genre/movie/list', {
+      params: {
+        'api_key': API_KEY,
+        language: this.language,
+      }
+    }).then(genere => this.genreMovies = genere.data.genres);
+
   },
 
   methods:{
@@ -34,7 +86,7 @@ const myApp = new Vue({
           }
         }).then(mov => {
           this.movies = mov.data.results
-          console.log(mov.data.results);
+          // console.log(mov.data.results);
         });
 
         axios.get('https://api.themoviedb.org/3/search/tv', {
@@ -47,7 +99,7 @@ const myApp = new Vue({
         }).then(tv => {
           tv.data.results.forEach(movie => this.movies.push(movie));
           console.log(tv.data.results);
-          console.log(this.movies)
+          // console.log(this.movies)
         });
       };
     },
@@ -62,9 +114,39 @@ const myApp = new Vue({
         e.target.src = 'https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'
     },
 
+    //places overview in english when translation is not found TO BE FIXED!!!!!!
+    englishOverview(movie){
+      let movieId = movie.id;
+      let newOverview = '';
+      axios.get('https://api.themoviedb.org/3/find', {
+        params: {
+          external_id: movieId,
+          'api_key': API_KEY,
+          language: 'en-US',
+          external_source: 'imdb_id',
+        }
+      }).then(overview => newOverview = overview.data.results);
+      return movieId
+    },
+
+    selectHome(home){
+      axios.get(`https://api.themoviedb.org/3/movie/${home.url}`, {
+        params: {
+          'api_key': API_KEY,
+          language: this.language,
+        }
+      }).then(h => this.movies = h.data.results);
+      console.log(this.movies);
+    },
+
+    genreFilter(movie){
+      return this.genreMovies.filter(e => movie.genre_ids.includes(e.id));
+    },
+
   },
 
   computed: {
+
   },
 
 
